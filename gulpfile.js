83b244219c2907132b836ad6bcc74772bde7e1eb
kuangpeng <kuangpeng@leiphone.com>
2017-02-09 18:20:14 +0800

    add files

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require("gulp"),
+    handlebars = require("gulp-handlebars"),
+    wrap = require("gulp-wrap"),
+    declare = require("gulp-declare"),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify"),
+    rename = require('gulp-rename');
+
+gulp.task("handlebars-preCompile", function(){
+    var template_src = "src/js/templates/",
+        pre_template_src = "src/js/pre-templates/";
+
+    gulp.src(template_src+'*.handlebars')
+        .pipe(handlebars())
+        .pipe(wrap('Handlebars.template(<%= contents %>)'))
+        .pipe(declare({
+            namespace: 'Student.templates',
+            noRedeclare: true // Avoid duplicate declarations
+        }))
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest(pre_template_src))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest(pre_template_src));
+});
---
+++ src/js/pre-templates/templates.js
@@ -0,0 +1,25 @@
+this["Student"] = this["Student"] || {};
+this["Student"]["templates"] = this["Student"]["templates"] || {};
+this["Student"]["templates"]["person"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
+    return "	<p>"
+    + container.escapeExpression(container.lambda((depth0 != null ? depth0.name : depth0), depth0))
+    + "</p>\r\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<p>this is person info</p>\r\n<h1>"
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</h1>\r\n<p>"
+    + alias4(((helper = (helper = helpers.age || (depth0 != null ? depth0.age : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"age","hash":{},"data":data}) : helper)))
+    + "</p>\r\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.classes : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
+this["Student"]["templates"]["student"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<p>this is student info</p>\r\n<h1>"
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "</h1>\r\n<p>"
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</p>";
+},"useData":true});
